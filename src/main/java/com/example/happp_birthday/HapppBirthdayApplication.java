f9a89e9043e4afa3d3ffbfdc149b607942d2a6b9
Imesh Isuranga <imeshisuranga00@gmail.com>
2024-05-28 15:01:34 +0530

    first commit

---
+++ src/main/java/com/example/happp_birthday/HapppBirthdayApplication.java
@@ -0,0 +1,13 @@
+package com.example.happp_birthday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HapppBirthdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HapppBirthdayApplication.class, args);
+	}
+
+}
